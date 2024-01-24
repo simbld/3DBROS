6be8d7d03bab6605d8a56a61c34585c8dc792e19
Simon Bullado <102648783+simbld@users.noreply.github.com>
2024-01-24 05:34:16 +0100

    Stop tracking .history directory

--- .history/front/pages/_app_20240124031654.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
