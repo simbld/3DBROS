4c1a963764de8ac288fc2fd8107d903cefa49da2
simbld <simon-bullado_student2022@wilder.school>
2024-01-26 06:37:12 +0100

    Setting up Jest for Nest

---
+++ back/jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from '@jest/types'
+const config: Config.InitialOptions = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+}
+
+export default config
