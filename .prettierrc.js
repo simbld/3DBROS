7969639ff18b359d17c846a7d7745fe80d13cc32
simbld <simon-bullado_student2022@wilder.school>
2024-01-29 03:29:25 +0100

    Configure front and back simultaneously

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,7 @@
 module.exports = {
-	semi: false,
-	trailingComma: 'all',
-	singleQuote: true,
-	printWidth: 80,
-	tabWidth: 2,
-	jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  singleQuote: false,
+  printWidth: 80,
+  tabWidth: 2
 };
