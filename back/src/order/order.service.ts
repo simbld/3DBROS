6127b7a892af7dc3c18f2f7c5b1783923d0b6571
simbld <simon-bullado_student2022@wilder.school>
2024-02-04 02:42:21 +0100

    feat: Integrate Socket.IO for Real-Time Order Management

--- back/src/order/order.service.ts
+++ back/src/order/order.service.ts
@@ -1,34 +1,39 @@
+/**
+ * Order management service for the application.
+ * This service provides CRUD functionality to manipulate order data
+ * stored in MongoDB. It allows you to create new orders, recover one or more
+ * orders, update an existing order, and delete an order.
+ *
+ * Uses Mongoose for interaction with the MongoDB database, relying on the `Order` model.
+ * Each method in this service is designed to be used by application controllers,
+ * providing a layer of abstraction between business logic and data access.
+ *
+ * @class OrderService
+ * @constructor
+ * @property {Model<Order>} orderModel - Mongoose model for the `Order` entity.
+ */
+
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
+import { Order } from "./entities/order.schema";
 import { Model } from "mongoose";
-import { CreateOrderInput } from "./dto/create-order.input";
-import { UpdateOrderInput } from "./dto/update-order.input";
-import { Order } from "./schemas/order.schema";
+import { OrderGateway } from "./order.gateway";
 
 @Injectable()
 export class OrderService {
-  constructor(@InjectModel(Order.name) private orderModel: Model<Order>) {}
+  constructor(
+    @InjectModel(Order.name) private orderModel: Model<Order>,
+    private orderGateway: OrderGateway,
+  ) {}
 
-  async create(createOrderInput: CreateOrderInput): Promise<Order> {
-    const createdOrder = new this.orderModel(createOrderInput);
-    return createdOrder.save();
+  async getAll(): Promise<Order[]> {
+    return this.orderModel.find({});
   }
 
-  async findAll(): Promise<Order[]> {
-    return this.orderModel.find().exec();
-  }
+  async create(orderData: Order): Promise<Order> {
+    const newOrder = await this.orderModel.create(orderData);
+    await newOrder.save();
 
-  async findOne(id: string): Promise<Order> {
-    return this.orderModel.findById(id).exec();
-  }
-
-  async update(id: string, updateOrderInput: UpdateOrderInput): Promise<Order> {
-    return this.orderModel
-      .findByIdAndUpdate(id, updateOrderInput, { new: true })
-      .exec();
-  }
-
-  async remove(id: string): Promise<Order> {
-    return this.orderModel.findByIdAndDelete(id).exec();
+    return newOrder;
   }
 }
