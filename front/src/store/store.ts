9f6c992d67295a8899639b19cf84bbdfd4c80635
simbld <simon-bullado_student2022@wilder.school>
2024-09-05 04:24:06 +0200

    chore: Update store configuration to include API slices for product, cart, order, and user

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -1,18 +1,29 @@
+// store.ts
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import { apiSlice } from "../services/apiSlice";
-import authReducer from "./slices/authSlice";
+import { productApiSlice } from "@services/productApiSlice";
+import { cartApiSlice } from "@services/cartApiSlice";
+import { orderApiSlice } from "@services/orderApiSlice";
+import { userApiSlice } from "@services/userApiSlice";
+import { authReducer } from "@reducers/authSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    [apiSlice.reducerPath]: apiSlice.reducer,
+    [productApiSlice.reducerPath]: productApiSlice.reducer,
+    [cartApiSlice.reducerPath]: cartApiSlice.reducer,
+    [orderApiSlice.reducerPath]: orderApiSlice.reducer,
+    [userApiSlice.reducerPath]: userApiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
+    getDefaultMiddleware().concat(
+      productApiSlice.middleware,
+      cartApiSlice.middleware,
+      orderApiSlice.middleware,
+      userApiSlice.middleware,
+    ),
 });
 
-// Configure the API slice to listen for lifecycle events
 setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
