d6291b39c50335185d862a38efd40b521a1aea0b
simbld <simon-bullado_student2022@wilder.school>
2024-09-12 10:56:21 +0200

    chore: Update store configuration to use redux-persist for state persistence

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -1,22 +1,70 @@
-// store.ts
-import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/query";
-import { productApiSlice } from "@services/productApiSlice";
 import { cartApiSlice } from "@services/cartApiSlice";
 import { orderApiSlice } from "@services/orderApiSlice";
+import { productApiSlice } from "@services/productApiSlice";
 import { userApiSlice } from "@services/userApiSlice";
 import { authReducer } from "@reducers/authSlice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import {
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  PERSIST,
+  FLUSH,
+  PAUSE,
+  REHYDRATE,
+} from "redux-persist";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+
+// Création d'un stockage factice pour éviter les erreurs côté serveur
+const createNoopStorage = () => {
+  return {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    getItem(_key: string) {
+      return Promise.resolve(null);
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    setItem(_key: string, _value: any) {
+      return Promise.resolve();
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    removeItem(_key: string) {
+      return Promise.resolve();
+    },
+  };
+};
+
+// Utilisation du stockage factice côté serveur et localStorage côté client
+const storage =
+  typeof window !== "undefined"
+    ? createWebStorage("local")
+    : createNoopStorage();
+
+// Configuration de persist
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  [productApiSlice.reducerPath]: productApiSlice.reducer,
+  [cartApiSlice.reducerPath]: cartApiSlice.reducer,
+  [orderApiSlice.reducerPath]: orderApiSlice.reducer,
+  [userApiSlice.reducerPath]: userApiSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    [productApiSlice.reducerPath]: productApiSlice.reducer,
-    [cartApiSlice.reducerPath]: cartApiSlice.reducer,
-    [orderApiSlice.reducerPath]: orderApiSlice.reducer,
-    [userApiSlice.reducerPath]: userApiSlice.reducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(
       productApiSlice.middleware,
       cartApiSlice.middleware,
       orderApiSlice.middleware,
@@ -28,3 +76,6 @@ setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type RootStateProps = ReturnType<typeof rootReducer>;
+export default rootReducer;
+export const persistor = persistStore(store);
