661fd34e6c2beb3045470089f0d270bc8ebe7502
simbld <simon-bullado_student2022@wilder.school>
2024-09-26 04:04:27 +0200

    Refactoring the date validation pipe to turn a string into a valid Date object

--- back/src/pipes/month-validation.pipe.ts
+++ back/src/pipes/month-validation.pipe.ts
@@ -1,6 +1,12 @@
 import { PipeTransform, BadRequestException } from "@nestjs/common";
 
-export class MonthValidationPipe implements PipeTransform {
+export class MonthValidationPipe implements PipeTransform<string, number> {
+  /**
+   * Transforms a string into a valid month (number).
+   * @param {string} value - The input string to transform.
+   * @returns {number} - The parsed month as a number.
+   * @throws {BadRequestException} - If the input is not a valid month.
+   */
   transform(value: string): number {
     const month = parseInt(value, 10);
     if (isNaN(month) || month < 1 || month > 12) {
