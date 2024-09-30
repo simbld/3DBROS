18262ee91c5803767a22a5880e8d125fc604facb
simbld <simon-bullado_student2022@wilder.school>
2024-09-30 03:22:20 +0200

    Update main.ts to use dynamic port for application listening

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as dotenv from "dotenv";
 
-dotenv.config({ path: "../.env" });
+dotenv.config({ path: "back/.env" });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  await app.listen(process.env.NEST_PUBLIC_API_URL);
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
