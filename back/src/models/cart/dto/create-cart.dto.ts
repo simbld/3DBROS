0fce09587be75294ae1fd75436ac108a022ff28c
simbld <simon-bullado_student2022@wilder.school>
2024-10-04 09:32:28 +0200

    Refactoring the cart creation DTO to add cart item validation

--- back/src/models/cart/dto/create-cart.dto.ts
+++ back/src/models/cart/dto/create-cart.dto.ts
@@ -1,19 +1,23 @@
-import { IsInt, IsArray, IsNotEmpty } from "class-validator";
+import { IsInt, IsArray, IsNotEmpty, IsPositive } from "class-validator";
 import { Type } from "class-transformer";
-import { CartItem } from "@entityCartItem/cart-item.entity";
+import { CartItem } from "@entityCartItem";
+import { Field, Int } from "@nestjs/graphql";
 
 /**
  * DTO for creating a Cart.
  * This DTO is used to validate the input when creating a new cart.
  */
 export class CreateCartDto {
+  @Field(() => Int)
   @IsInt()
+  @IsPositive({ message: "User ID must be a positive integer" })
   @IsNotEmpty({ message: "User ID is required" })
   userId: number;
 
+  @Field(() => [CartItem])
   @IsArray()
-  @IsNotEmpty({ message: "Cart items are required" })
   @Type(() => CartItem)
+  @IsNotEmpty({ message: "Cart must have at least one item" })
   cartItems: CartItem[];
 
   constructor(userId: number, cartItems: CartItem[]) {
