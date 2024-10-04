a111fa4481d4572448fdbae59dfed04ff4788cdf
simbld <simon-bullado_student2022@wilder.school>
2024-10-04 09:32:49 +0200

    Refactoring the cart update DTO to add cart item validation

--- back/src/models/cart/dto/update-cart.dto.ts
+++ back/src/models/cart/dto/update-cart.dto.ts
@@ -1,19 +1,27 @@
-import { IsInt, IsOptional, IsArray, IsNotEmpty } from "class-validator";
+import { IsInt, IsOptional, IsArray, IsPositive } from "class-validator";
 import { Type } from "class-transformer";
-import { CartItem } from "@entityCartItem/cart-item.entity";
+import { CartItem } from "@entityCartItem";
+import { Field, Int } from "@nestjs/graphql";
 
 /**
  * DTO for updating a Cart.
  * This DTO is used to validate the input when updating an existing cart.
  */
 export class UpdateCartDto {
+  @Field(() => Int)
   @IsInt()
-  @IsOptional() // Le userId est facultatif dans le cas d'une mise à jour
+  @IsOptional()
+  @IsPositive({ message: "User ID must be a positive integer" })
   userId?: number;
 
+  @Field(() => [CartItem])
   @IsArray()
-  @IsOptional() // Les items peuvent être facultatifs lors d'une mise à jour
-  @IsNotEmpty({ message: "Cart items cannot be empty" })
+  @IsOptional()
   @Type(() => CartItem)
   cartItems?: CartItem[];
+
+  constructor(userId?: number, cartItems?: CartItem[]) {
+    this.userId = userId;
+    this.cartItems = cartItems;
+  }
 }
