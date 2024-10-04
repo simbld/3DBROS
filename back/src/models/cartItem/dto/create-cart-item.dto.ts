6eaac1e8376a001a6e79941e7a008b990a51e18f
simbld <simon-bullado_student2022@wilder.school>
2024-10-04 09:33:05 +0200

    Refactoring the cart item creation DTO to add cart item validation

--- back/src/models/cartItem/dto/create-cart-item.dto.ts
+++ back/src/models/cartItem/dto/create-cart-item.dto.ts
@@ -6,26 +6,32 @@ import {
   IsPositive,
 } from "class-validator";
 import { OrderStatus } from "@enums/order-status.enum";
+import { Field, Float, Int } from "@nestjs/graphql";
 
 /**
  * DTO for creating a CartItem.
  * This DTO is used to validate the input when creating a new cart item.
  */
 export class CreateCartItemDto {
+  @Field(() => Int)
   @IsInt()
   @IsNotEmpty({ message: "Product ID is required" })
+  @IsPositive({ message: "Product ID must be a positive integer" })
   productId: number;
 
+  @Field(() => Float)
   @IsNumber()
   @IsPositive({ message: "Price must be a positive number" })
   @IsNotEmpty({ message: "Price is required" })
   price: number;
 
+  @Field(() => Int)
   @IsInt()
   @IsPositive({ message: "Quantity must be a positive integer" })
   @IsNotEmpty({ message: "Quantity is required" })
   quantity: number;
 
+  @Field(() => OrderStatus)
   @IsEnum(OrderStatus, { message: "Invalid order status" })
   @IsNotEmpty({ message: "Order status is required" })
   status: OrderStatus;
